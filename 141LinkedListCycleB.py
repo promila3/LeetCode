68a8fcb8b946a58b8319bbb99e98d5dd277c030d
promila3 <promila3@gmail.com>
2021-02-04 13:04:08 -0800

    Create 141LinkedListCycleB.py

---
+++ 141LinkedListCycleB.py
@@ -0,0 +1,12 @@
+    #https://leetcode.com/problems/linked-list-cycle/
+    def hasCycle(self, head:ListNode) -> bool:
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
