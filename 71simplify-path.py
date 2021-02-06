80b4f1a90edcd8b667c1a427bbcdd2ebcbed840c
promila3 <promila3@gmail.com>
2021-02-06 13:31:14 -0800

    Create 71simplify-path.py

---
+++ 71simplify-path.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/simplify-path/
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for portion in path.split("/"):
+            if portion == "..":
+                if stack:
+                    stack.pop()
+            elif portion == "." or not portion:
+                continue
+            else:
+                stack.append(portion)
+        final_str ="/"+"/".join(stack)
+        return final_str
