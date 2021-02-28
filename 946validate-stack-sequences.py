6ceec1a0bde0cd5e360fe76031241e6b54f0d9fb
promila3 <promila3@gmail.com>
2021-02-28 13:05:09 -0800

    Create 946validate-stack-sequences.py

---
+++ 946validate-stack-sequences.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/validate-stack-sequences/
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+
+        j = 0
+        stack = []
+        for x in pushed:
+            stack.append(x)
+            while stack and j < len(popped) and stack[-1] == popped[j]:
+                stack.pop()
+                j+=1
+        return j == len(popped)
