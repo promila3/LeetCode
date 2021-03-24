d925d39c1f39dcda4d85b05b3fae9ccf5e8ccaa0
promila3 <promila3@gmail.com>
2021-03-24 00:01:07 -0700

    Create 9233sum-with-multiplicity.py

---
+++ 9233sum-with-multiplicity.py
@@ -0,0 +1,33 @@
+#https://leetcode.com/problems/3sum-with-multiplicity/
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        MOD = 10**9+7
+        ans = 0
+        arr.sort()
+        for i, x in enumerate(arr):
+            T = target - arr[i]
+            j, k = i+1, len(arr) - 1
+            while j < k:
+                if arr[j] + arr[k] < T:
+                    j+=1
+                elif arr[j] + arr[k] > T:
+                    k -=1
+                elif arr[j] != arr[k]:
+                    left,right = 1, 1
+                    while j+1 < k and arr[j] == arr[j+1]:
+                        left+=1
+                        j+=1
+                    while k -1 > j and arr[k] == arr[k-1]:
+                        right +=1
+                        k -=1
+                    ans += left * right
+                    ans %= MOD
+                    j+=1
+                    k-=1
+                else:
+                    ans += (k-j+1) *(k-j) //2
+                    ans %= MOD
+                    break
+                    
+        
+        return ans
