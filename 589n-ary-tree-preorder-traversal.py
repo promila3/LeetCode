ebf433a87639f1085012ed1c1bbd94b38ad061c7
promila3 <promila3@gmail.com>
2021-04-20 21:50:06 -0700

    Create 589n-ary-tree-preorder-traversal.py

---
+++ 589n-ary-tree-preorder-traversal.py
@@ -0,0 +1,18 @@
+"""
+# https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root == None:
+            return []
+        result = []
+        result.append(root.val)
+        for child in root.children:
+            result += self.preorder(child)
+        return result
