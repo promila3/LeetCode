5a419ee68f1ae877c879caf76895e01781f8dbdb
promila3 <promila3@gmail.com>
2025-01-23 13:03:15 -0800

    Create 1636SortArraysByIncreasingFrequency.py

---
+++ 1636SortArraysByIncreasingFrequency.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/sort-array-by-increasing-frequency/description/
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freq = Counter(nums)
+        return sorted(nums, key=lambda x: (freq[x], -x))
