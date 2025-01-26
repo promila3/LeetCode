781f3f26a2c63460ec0ca50c6891a957b9b09a0a
promila3 <promila3@gmail.com>
2025-01-25 23:02:58 -0800

    Update 169MajorityElement.py

--- 169MajorityElement.py
+++ 169MajorityElement.py
@@ -15,4 +15,17 @@ class Solution:
         for i in nums2:
             temp = nums.count(i)
             if temp > halfFr:
-                return i
+                return I
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res = majority = 0
+        
+        for n in nums:
+            if majority == 0:
+                res = n
+            
+            majority += 1 if n == res else -1
+        
+        return res
