413c94390ba750d7d878539fe7f1c0833a09a8a6
promila3 <promila3@gmail.com>
2025-01-29 18:56:23 -0800

    Update 1636SortArraysByIncreasingFrequency.py

--- 1636SortArraysByIncreasingFrequency.py
+++ 1636SortArraysByIncreasingFrequency.py
@@ -3,3 +3,10 @@ class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         freq = Counter(nums)
         return sorted(nums, key=lambda x: (freq[x], -x))
+
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        count: List[int] = [ 0 for _ in range(201)]
+        for num in nums:
+            count[num + 100] +=1
+        nums.sort(key=lambda val: (count[val+100], -val))
+        return nums
