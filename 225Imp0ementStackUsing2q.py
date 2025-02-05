e699806fa0065271f195e6d68f464aca9eddd3cd
promila3 <promila3@gmail.com>
2025-02-04 16:13:27 -0800

    Create 225Imp0ementStackUsing2q.py

---
+++ 225Imp0ementStackUsing2q.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/implement-stack-using-queues/description/ 
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        popped_val =self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
+
+        return popped_val
+
+    def top(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        top_val = self.q1[0]
+        self.q2.append(self.q1.popleft())
+        self.q1, self.q2 = self.q2, self.q1
+
+        return top_val
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
