bf91f632725c67f5f4223b22b2af78f62e4b4bf9
promila3 <promila3@gmail.com>
2025-05-10 16:42:13 -0700

    Create 3342MinTimeLastRoom.py

---
+++ 3342MinTimeLastRoom.py
@@ -0,0 +1,49 @@
+#https://leetcode.com/problems/find-minimum-time-to-reach-last-room-ii
+'''
+Let n and m be the number of rows and columns in moveTime, respectively.
+
+Time complexity: O(nmlog(nm)).
+There are nm points and O(nm) edges. We implement Dijkstra's algorithm using a min-heap, performing at most O(nm) insertions and deletions. Since each heap operation takes O(log(nm)) time, the overall time complexity is O(nmlog(nm)).
+
+Space complexity: O(nm).
+'''
+class State:
+    def __init__(self, x, y, dis):
+        self.x = x
+        self.y = y
+        self.dis = dis
+
+    def __lt__(self, other):
+        return self.dis < other.dis
+
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n = len(moveTime)
+        m = len(moveTime[0])
+        inf = float("inf")
+        d = [[inf] * m for _ in range(n)]
+        v = [[0] * m for _ in range(n)]
+
+        dirs = [(1,0), (-1, 0), (0, 1), (0, -1)]
+        d[0][0] = 0
+        q = []
+        heapq.heappush(q, State(0, 0, 0))
+        
+        while q:
+         
+            s  = heapq.heappop(q)
+            if v[s.x][s.y]: 
+                continue
+            if s.x == n -1 and s.y == m -1:
+                break
+            v[s.x][s.y] = 1
+            for dx, dy in dirs:
+                nx, ny = s.x + dx, s.y + dy
+                if not (0 <= nx < n and 0 <= ny < m):
+                    continue
+                dist = max(d[s.x][s.y], moveTime[nx][ny]) + (s.x + s.y) % 2  + 1
+                if d[nx][ny] > dist:
+                    d[nx][ny] = dist
+                    heapq.heappush(q, State(nx, ny, dist))
+
+        return d[n-1][m - 1]
